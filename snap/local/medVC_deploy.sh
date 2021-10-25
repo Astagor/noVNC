ee2f94d6d9c9aaa8e5e44c46469d75d7467df2e4
astagor <astagor@gmail.com>
2021-10-25 19:59:42 +0200

    Try to get jq to work v7

---
+++ snap/local/medVC_deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /snap/novnc/current
+mkdir -p /var/www/html/novnc
+cp *.php /var/www/html/novnc/
+cp -r medvc_extra_services /var/www/html/novnc/
+cp -r img /var/www/html/novnc/
+cp novnc_proxy /var/www/html/novnc/
+cp -r app /var/www/html/novnc/
+cp -r core /var/www/html/novnc/
+cp -r vendor /var/www/html/novnc/
