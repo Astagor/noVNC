6b7424465c0e92f4530b4f39448a0e3ee05ba823
astagor <astagor@gmail.com>
2023-02-15 11:13:05 +0100

    Eizo v3

--- medVC_deploy_no_snap.sh
+++ medVC_deploy_no_snap.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 cd /var/www/noVNC
-mkdir -p /var/www/html/novnc
-cp *.php /var/www/html/novnc/
-cp -r medvc_extra_services /var/www/html/novnc/
-cp -r img /var/www/html/novnc/
-cp -r app /var/www/html/novnc/
-cp -r core /var/www/html/novnc/
-cp -r vendor /var/www/html/novnc/
-chown -R www-data:www-data /var/www/html/novnc
-cp services/novnc.conf.medvc.eu.service /etc/systemd/system/novnc.conf.medvc.eu.service
-cp services/novnc2.conf.medvc.eu.service /etc/systemd/system/novnc2.conf.medvc.eu.service
-cp services/novnc3.conf.medvc.eu.service /etc/systemd/system/novnc3.conf.medvc.eu.service
-cp services/novnc.speakers.tnc21.geant.org.service /etc/systemd/system/novnc.speakers.tnc21.geant.org.service
+mkdir -p /var/www/html/medvc_remote
+cp index.php /var/www/html/medvc_remote/
+cp -r img /var/www/html/medvc_remote/
+cp -r app /var/www/html/medvc_remote/
+cp -r core /var/www/html/medvc_remote/
+cp -r vendor /var/www/html/medvc_remote/
+chown -R www-data:www-data /var/www/html/medvc_remote
+cp services/novnc.service /etc/systemd/system/novnc.service
 systemctl daemon-reload
-systemctl enable novnc.conf.medvc.eu.service
-systemctl enable novnc2.conf.medvc.eu.service
-systemctl enable novnc3.conf.medvc.eu.service
-systemctl enable novnc.speakers.tnc21.geant.org.service
-systemctl restart novnc.conf.medvc.eu.service
-systemctl restart novnc2.conf.medvc.eu.service
-systemctl restart novnc3.conf.medvc.eu.service
-systemctl restart novnc.speakers.tnc21.geant.org.service
+systemctl enable novnc.service
+systemctl restart novnc.service
